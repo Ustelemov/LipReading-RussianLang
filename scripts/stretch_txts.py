aabfe24e8530e869ae4f214aa2b3088eb00d989a
Ustelemov <58209083+Ustelemov@users.noreply.github.com>
2020-06-05 14:17:13 +0300

    Update and rename stretch_two_txts.py to stretch_txts.py

---
+++ scripts/stretch_txts.py
@@ -0,0 +1,22 @@
+import os
+import argparse
+import glob
+
+
+
+#Конфигурация параметров при вызове с консоли
+parser = argparse.ArgumentParser(usage='Stretch txt files in folder to txt')
+parser.add_argument('--i',dest="input_path",type=str,required=True,help="Path to files")
+parser.add_argument('--o',dest="out_filepath",type=str,required=True,help="Path to output txt file")
+
+
+args = parser.parse_args()
+
+input_path = args.input_path
+out_filepath = args.out_filepath
+
+
+with open(out_filepath,'w') as w: #Откроем файл для записи 
+  for f in glob.glob(input_path+'/*.txt'): #Найдем все txt файлы в папке
+    with open(f,'r') as r:
+      w.writelines(r.readlines()) #Запишем в файл прочитанные сторки (readlines) файла

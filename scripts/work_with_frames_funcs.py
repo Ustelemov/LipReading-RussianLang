4dce486954ece0767c5e6ae8671c38e92bd9f4e8
Ustelemov <58209083+Ustelemov@users.noreply.github.com>
2021-05-28 12:33:29 +0300

    Update work_with_frames_funcs.py

--- scripts/work_with_frames_funcs.py
+++ scripts/work_with_frames_funcs.py
@@ -1,190 +1,187 @@
-import face_recognition
+import tensorflow as tf
 import cv2
 import numpy as np
-from PIL import Image,ImageDraw,ImageFont
+import dlib
 
-#Функция для получения cv2-кадров лиц (возвращается список cv2 кадров) с отступом margin для границ лица
-def get_faces_from_frame(cv2_frame, margin=10):
-  # Перевед изображение из BGR-цвета (который использует OpenCV) в RGB (который необходим для face_recognition)
-  rgb_frame = frame[:, :, ::-1]
-  #Параметры кадра
-  height, width, channels = cv2_frame.shape
+class SSDFaceDetector():
 
-  #Массив координат квадрата лица: верхней угол, правый угол, нижний угол, левый угол (top,right,bottom,left)
-  face_locations = face_recognition.face_locations(rgb_frame)
+    def __init__(self,
+                 det_threshold=0.3, input_width = 240, input_height = 180,
+                 model_path='/content/LipReading-RussianLang/models/frozen_inference_graph.pb'):
 
-  face_frames = []
-
-  #Установим границы для сабфрейма-лица, проверив на выход за границы изображения
-  for top,right,bottom,left in face_locations:
-    yMin = top - margin if top - margin >= 0 else 0
-    yMax = bottom + margin if bottom + margin < height  else height-1
-
-    xMin = left - margin if left - margin >= 0 else 0
-    xMax = right + margin if right + margin < width else width -1
-  
-    face_frames.append(cv2_frame[yMin:yMax,xMin:xMax])
-
-  return face_frames
-
-#Функция нормализации - может быть использована как для губ, так и для лица:
-#Не будем переименовывать переменные
-#В случае губ передается крайняя левая координата, в случае лица - координаты глаз
-#cv2_face_frame - кадр с лицом (одним лицом)
-#leftPts - точки левого глаза (у губ - крайняя левая точка)
-#rightPts - точки правого глаза (у губ - крайняя правая точка)
-#desiredLeftEye - желаемая позиция центроида (центра) левого глаза  (у губ - желаемая позиция левой точки)
-#desiredFaceWidth - требуемый выходной размер изображения 
-#desiredFaceHeight - требуемый выходной размер изображения 
+        self.det_threshold = det_threshold
+        self.input_width = input_width
+        self.input_height = input_height
+        self.detection_graph = tf.Graph()
 
 
-def align(cv2_face_frame, leftEyePts,rightEyePts,desiredLeftEye = (0.35,0.35),desiredFaceWidth=256,desiredFaceHeight=256):
-  
-  leftEyePts = np.asarray(leftEyePts)
-  rightEyePts = np.asarray(rightEyePts)
+        with self.detection_graph.as_default():
+            od_graph_def = tf.compat.v1.GraphDef()
+            with tf.io.gfile.GFile(model_path, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
 
-  #вычисляем центр масс каждого глаза
-  leftEyeCenter = leftEyePts.mean(axis=0).astype("int")
-  rightEyeCenter = rightEyePts.mean(axis=0).astype("int")
+            self.sess = tf.compat.v1.Session(graph=self.detection_graph)
 
-  #вычисляем угол между центроидами глаз
-  dY = rightEyeCenter[1] - leftEyeCenter[1]
-  dX = rightEyeCenter[0] - leftEyeCenter[0]
-
-  #Изначальная формула: angle = np.degrees(np.arctan2(dY, dX))-180. -180 убрал за ненадобностью?
-  angle = np.degrees(np.arctan2(dY, dX))
-
-  #Вычисляем желательную X координату правого глаза, основываясь на желательной координате левого глаза
-  desiredRightEyeX = 1.0 - desiredLeftEye[0]
-
-  #вычисляем величину масштабирования
-  #c помошью вычисления отношения между текущим расстоянием между глазами
-  #и желательным расстоянием между глазами 
-  dist = np.sqrt((dX ** 2) + (dY ** 2))
-  desiredDist = (desiredRightEyeX - desiredLeftEye[0])
-  desiredDist *= desiredFaceWidth
-  scale = desiredDist / dist
-
-  #вычисляем центр (х,y)-координаты (медианную точку)
-  #между двумя глазами
-  eyesCenter = ((leftEyeCenter[0] + rightEyeCenter[0]) // 2,
-  (leftEyeCenter[1] + rightEyeCenter[1]) // 2)
-
-  #Создаем матрицу преобразования
-  M = cv2.getRotationMatrix2D(eyesCenter, angle, scale)
-
-  #Изменяем компоненты матрицы
-  tX = desiredFaceWidth * 0.5
-  tY = desiredFaceHeight * desiredLeftEye[1]
-  M[0, 2] += (tX - eyesCenter[0])
-  M[1, 2] += (tY - eyesCenter[1])
-
-  #применяем аффиное преобразование
-  (w, h) = (desiredFaceWidth,desiredFaceHeight)
-  output = cv2.warpAffine(cv2_face_frame, M, (w, h),flags=cv2.INTER_CUBIC)
-
-  return output
-
-#Функция получение массива выравненных лиц
-#для этого потребуется получить массив лиц с помощью get_faces_from_frame (с каким-то margin) 
-#cv2_frame - cv2 кадр с лицами
-#desiredLeftEye - желаемая позиция центроида (центра) левого глаза
-#desiredFaceWidth - требуемый выходной размер изображения (лица)
-#desiredFaceHeight - требуемый выходной размер изображения (лица)
-
-def get_aligned_faces(cv2_frame,margin=10,desiredLeftEye = (0.35,0.35),desiredFaceWidth=256,desiredFaceHeight=256):
-  #список выровненных кадров лиц
-  aligned_faces = []
-
-  for f in get_faces_from_frame(cv2_frame,margin): 
-    rgb_frame = f[:, :, ::-1]
-    face_locations = face_recognition.face_landmarks(rgb_frame)
     
-    face_aligned = align_face(f,face_locations[0]['left_eye'],face_locations[0]['right_eye'],desiredLeftEye,desiredFaceWidth,desiredFaceHeight)
-    aligned_faces.append(face_aligned)
+    def detect_face(self, image):
 
-  return aligned_faces
+        h, w,c = image.shape
 
-def draw_point(cv2_frame,point):
-  cv2.circle(cv2_frame, point, 1, (255, 255, 255), -1)  
+        image_res = cv2.resize(image, (self.input_width, self.input_height))
+        image_np = cv2.cvtColor(image_res, cv2.COLOR_BGR2RGB)
 
-def draw_points(cv2_frame,points):
-	for (x, y) in points:
-		draw_point(cv2_frame,(x,y))
-  
-#Функция возвращающая точки нижний губы (bottom_lip) и верхней губы (top_lip) для каждого лица на кадре 
-def get_lips_points(cv2_frame):
-  rgb_frame = cv2_frame[:, :, ::-1]
-  face_locations = face_recognition.face_landmarks(rgb_frame)
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        image_tensor = self.detection_graph.get_tensor_by_name(
+            'image_tensor:0')
 
-  lips = []
+        boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
 
-  for loc in face_locations:
-    lips.append({'bottom_lip':loc['bottom_lip'],'top_lip':loc['top_lip']})
-  return lips
+        scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        classes = self.detection_graph.get_tensor_by_name(
+            'detection_classes:0')
+        num_detections = self.detection_graph.get_tensor_by_name(
+            'num_detections:0')
 
-#Функция рисующая точки губ для каждого лица
-def draw_lips_points(cv2_frame):
-  for lips in get_lips_points(cv2_frame):
-    draw_points(cv2_frame,lips['bottom_lip']+lips['top_lip'])
+        (boxes, scores, classes, num_detections) = self.sess.run([boxes, scores, classes, num_detections],feed_dict={image_tensor: image_np_expanded})
 
-#Здесь мы не можем создать отдельную функцию для получения губ с margin
-#А потом искать в них точки, только по губам он точки не найдет
-def get_aligned_lips(cv2_frame,margin=50,desiredLeftLip = (0.05,0.5),desiredLipWidth=320,desiredLipHeight=240):
-  
-  aligned_lips = []
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
 
-  face_locations = get_lips_points(cv2_frame)
+        filtered_score_index = np.argwhere(
+            scores >= self.det_threshold).flatten()
 
-  #Параметры кадра
-  height, width, channels = cv2_frame.shape
-  
-  for loc in face_locations:
-    points = loc['bottom_lip']+loc['top_lip']
+        selected_boxes = boxes[filtered_score_index]
+
+        faces = np.array([[
+            int(x1 * w),
+            int(y1 * h),
+            int(x2 * w),
+            int(y2 * h),
+        ] for y1, x1, y2, x2 in selected_boxes])
+
+        return faces
+
+class DlibFaceAligner:
+    def __init__(self, landmarks_path='/content/LipReading-RussianLang/models/shape_predictor_68_face_landmarks.dat'):
+        # store the facial landmark predictor, desired output left
+        # eye position, and desired output face width + height
+        self.predictor = dlib.shape_predictor(landmarks_path)
+
+        self.detector = SSDFaceDetector()
+
+    def shape_to_np(shape, dtype="int"):
+      # initialize the list of (x, y)-coordinates
+      coords = np.zeros((shape.num_parts, 2), dtype=dtype)
+
+      # loop over all facial landmarks and convert them
+      # to a 2-tuple of (x, y)-coordinates
+      for i in range(0, shape.num_parts):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
+
+      # return the list of (x, y)-coordinates
+      return coords
+
+    def get_aligned_lips(self,cv2_frame,margin=50,desiredLeftLip = (0.05,0.5),desiredLipWidth=240,desiredLipHeight=240):
     
-    #Получим точки максимальные и минимальные по X и Y
-    xMax_p = max(points,key = lambda item: item[0])
-    xMin_p = min(points,key = lambda item: item[0])
-    
-    yMax_p = max(points,key = lambda item: item[1])
-    yMin_p = min(points,key = lambda item: item[1])
-    
-    #Удостоверимся, что не выходим за кадр
-    xMax_m = xMax_p[0]+margin if xMax_p[0]+margin<width else width-1
-    xMin_m = xMin_p[0]-margin if xMin_p[0]-margin>=0 else 0
+        coords = self.detector.detect_face(cv2_frame)
 
-    yMax_m = yMax_p[1]+margin if yMax_p[1]+margin<height else height-1
-    yMin_m = yMin_p[1]-margin if yMin_p[1]-margin>=0 else 0
+        x1, y1, x2, y2 = [int(c) for c in coords[0]]
+        rect = dlib.rectangle(left=x1, top=y1, right=x2, bottom=y2)
 
-    lips_frame = cv2_frame[yMin_m:yMax_m,xMin_m:xMax_m]
-
-    #Переведем координаты в координаты нового кадра
-    #От x_min отняли margin либо если расстояние до 0 была меньше margin, то эту разницу
-    left_x_convert = margin if xMin_p[0]>margin else xMin_p[0]
-
-    #Посмотрим какова была разница между y координатой крайней точки и минимальной y координатой
-    #И добавим marin, либо меньше как и с left_x_convert
-    left_y_convert = (xMin_p[1]-yMin_p[1])+margin if yMin_p[1]>margin else yMin_p[1]
+        # dlib works with gray, so as image we're waiting for bgr opencv captured image and convert that to
+        gray = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2GRAY)
 
 
-    #Теперь посмотрим какая разница была у правой точки с левой и добавим эту разницу
-    right_convert = (left_x_convert+(xMax_p[0]-xMin_p[0]),left_y_convert+(xMax_p[1]-xMin_p[1]))
+        # convert the landmark (x, y)-coordinates to a NumPy array
+        shape = self.predictor(gray, rect)
+        shape = shape_to_np(shape)
 
-    aligned_lips.append(align(lips_frame,[(left_x_convert,left_y_convert)],[right_convert],
-                              desiredLeftLip,desiredLipWidth,desiredLipHeight))
-  return aligned_lips
+        points = np.concatenate((shape[54:61], shape[64:68],shape[48:49],shape[49:54],shape[61:64]), axis=0)
+        #bottom_lip = shape[54:61]+shape[64:68]+shape[48]
+        #top_lip = shape[49:54]+shape[61:64]
 
-#Используется DejaVuSans.ttf, чтобы мочь писать и на русском
-def label_on_frame(cv2_frame,label,point=(10,10),fontsize=24,font_path='/content/DejaVuSans.ttf',color=(255,255,255,0)):
-  font = ImageFont.truetype(font_path, fontsize)
-  
-  rgb_frame = cv2_frame[:,:,::-1]
-  img_pil = Image.fromarray(rgb_frame)
+        
+        aligned_lips = []
 
-  draw = ImageDraw.Draw(img_pil)
-  draw.text(point, label , font=font, fill=color)
-  
-  frame_texted = np.asarray(img_pil)
-  frame_texted = frame_texted[:,:,::-1]
+        #Параметры кадра
+        height, width, channels = cv2_frame.shape
+        
+        xMax_p = max(points,key = lambda item: item[0])
+        xMin_p = min(points,key = lambda item: item[0])
+          
+        yMax_p = max(points,key = lambda item: item[1])
+        yMin_p = min(points,key = lambda item: item[1])
+          
+        #Удостоверимся, что не выходим за кадр
+        xMax_m = xMax_p[0]+margin if xMax_p[0]+margin<width else width-1
+        xMin_m = xMin_p[0]-margin if xMin_p[0]-margin>=0 else 0
 
-  return frame_texted
+        yMax_m = yMax_p[1]+margin if yMax_p[1]+margin<height else height-1
+        yMin_m = yMin_p[1]-margin if yMin_p[1]-margin>=0 else 0
+
+        lips_frame = cv2_frame[yMin_m:yMax_m,xMin_m:xMax_m]
+
+        #Переведем координаты в координаты нового кадра
+        #От x_min отняли margin либо если расстояние до 0 была меньше margin, то эту разницу
+        left_x_convert = margin if xMin_p[0]>margin else xMin_p[0]
+
+        #Посмотрим какова была разница между y координатой крайней точки и минимальной y координатой
+        #И добавим marin, либо меньше как и с left_x_convert
+        left_y_convert = (xMin_p[1]-yMin_p[1])+margin if yMin_p[1]>margin else yMin_p[1]
+
+
+        #Теперь посмотрим какая разница была у правой точки с левой и добавим эту разницу
+        right_convert = (left_x_convert+(xMax_p[0]-xMin_p[0]),left_y_convert+(xMax_p[1]-xMin_p[1]))
+
+        return align.align(lips_frame,[(left_x_convert,left_y_convert)],[right_convert],
+                                desiredLeftLip,desiredLipWidth,desiredLipHeight)
+
+
+    def align(self,cv2_face_frame, leftEyePts,rightEyePts,desiredLeftEye = (0.35,0.35),desiredFaceWidth=256,desiredFaceHeight=256):
+        
+        leftEyePts = np.asarray(leftEyePts)
+        rightEyePts = np.asarray(rightEyePts)
+
+        #вычисляем центр масс каждого глаза
+        leftEyeCenter = leftEyePts.mean(axis=0).astype("int")
+        rightEyeCenter = rightEyePts.mean(axis=0).astype("int")
+
+        #вычисляем угол между центроидами глаз
+        dY = rightEyeCenter[1] - leftEyeCenter[1]
+        dX = rightEyeCenter[0] - leftEyeCenter[0]
+
+        #Изначальная формула: angle = np.degrees(np.arctan2(dY, dX))-180. -180 убрал за ненадобностью?
+        angle = np.degrees(np.arctan2(dY, dX))
+
+        #Вычисляем желательную X координату правого глаза, основываясь на желательной координате левого глаза
+        desiredRightEyeX = 1.0 - desiredLeftEye[0]
+
+        #вычисляем величину масштабирования
+        #c помошью вычисления отношения между текущим расстоянием между глазами
+        #и желательным расстоянием между глазами 
+        dist = np.sqrt((dX ** 2) + (dY ** 2))
+        desiredDist = (desiredRightEyeX - desiredLeftEye[0])
+        desiredDist *= desiredFaceWidth
+        scale = desiredDist / dist
+
+        #вычисляем центр (х,y)-координаты (медианную точку)
+        #между двумя глазами
+        eyesCenter = ((leftEyeCenter[0] + rightEyeCenter[0]) // 2,
+        (leftEyeCenter[1] + rightEyeCenter[1]) // 2)
+
+        #Создаем матрицу преобразования
+        M = cv2.getRotationMatrix2D(eyesCenter, angle, scale)
+
+        #Изменяем компоненты матрицы
+        tX = desiredFaceWidth * 0.5
+        tY = desiredFaceHeight * desiredLeftEye[1]
+        M[0, 2] += (tX - eyesCenter[0])
+        M[1, 2] += (tY - eyesCenter[1])
+
+        #применяем аффиное преобразование
+        (w, h) = (desiredFaceWidth,desiredFaceHeight)
+        output = cv2.warpAffine(cv2_face_frame, M, (w, h),flags=cv2.INTER_CUBIC)
+
+        return output
